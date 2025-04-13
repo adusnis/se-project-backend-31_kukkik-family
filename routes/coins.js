ec723b9da71cc3935a879b6364ee9df88ff6930f
Ayaiko <graind1150@gmail.com>
2025-04-13 17:01:06 +0700

    fix: change path of add and deduct coin API

--- routes/coins.js
+++ routes/coins.js
@@ -5,9 +5,8 @@ const router = express.Router();
 
 const {protect, authorize} = require('../middleware/auth');
 
-router('/')
-    .get(protect, authorize('admin', 'user', 'renter'), getCoins)
-    .post(protect, authorize('admin', 'user', 'renter'), addCoins)
-    .get(protect, authorize('admin', 'user', 'renter'), deductCoins)
+router('/').get(protect, authorize('admin', 'user', 'renter'), getCoins)
+router('/add').post(protect, authorize('admin', 'user', 'renter'), addCoins)
+router('/deduct').post(protect, authorize('admin', 'user', 'renter'), deductCoins)
     
 module.exports = router;
