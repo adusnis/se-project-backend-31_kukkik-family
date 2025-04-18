c48a1d62b2d6c2240f05254c2a7e593688771738
Ayaiko <graind1150@gmail.com>
2025-04-18 21:50:17 +0700

    change transfer money from put to post

--- routes/coins.js
+++ routes/coins.js
@@ -9,7 +9,7 @@ router.route('/').get(protect, authorize('admin', 'user', 'renter'), getCoins)
 router.route('/add').put(protect, authorize('admin', 'user', 'renter'), addCoins)
 router.route('/deduct').put(protect, authorize('admin', 'user', 'renter'), deductCoins)
 router.route('/getQR').post(protect, authorize('admin', 'user', 'renter'), getQR)
-router.route('/confirm').post(protect, authorize('admin', 'user', 'renter'), transferNetRevenue, addCoins)
+router.route('/confirm').put(protect, authorize('admin', 'user', 'renter'), transferNetRevenue, addCoins)
 router.route('/redeem/:code').get(redeemCoins, addCoins)
 
 module.exports = router;
