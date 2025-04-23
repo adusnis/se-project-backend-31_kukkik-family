77280784351187fecace0010fd0bf401d7a9198e
Ayaiko <graind1150@gmail.com>
2025-04-23 20:27:42 +0700

    change IdCardPicture to idCardPicture for consistency

--- models/User.js
+++ models/User.js
@@ -46,7 +46,7 @@ const UserSchema=new mongoose.Schema({
         type: String,
         required: false
     },
-    IdCardPicture: {
+    idCardPicture: {
         type: String,
         required: false
     },
